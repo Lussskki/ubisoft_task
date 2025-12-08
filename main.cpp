8a629ff5c1f0f48dc1056825a2a869fc4a61b133
Luka <lukaguledani123@gmail.com>
2025-12-08 18:26:28 +0400

    create a window

--- main.cpp
+++ main.cpp
@@ -2,9 +2,36 @@
 // Created by Luka on 08.12.2025.
 //
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-using namespace std;
 
 int main() {
-    cout<<"Hello World!"<<endl;
+    if (!glfwInit()) {
+        std::cout << "Failed to init GLFW\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to init GLEW\n";
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
