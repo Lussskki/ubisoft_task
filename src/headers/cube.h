a247cdd7b22635d3c87cafe883608e98f4d76a1e
Luka <lukaguledani123@gmail.com>
2025-12-09 19:28:42 +0400

    change cube to explicit

--- src/headers/cube.h
+++ src/headers/cube.h
@@ -11,7 +11,7 @@ struct Cube
     GLuint VAO, VBO;
     glm::vec3 position;
 
-    Cube(const glm::vec3& pos = glm::vec3(0.0f));
+    explicit Cube(const glm::vec3& pos = glm::vec3(0.0f));
     ~Cube();
 
     void Draw(GLuint shaderProgram, GLint locModel, GLint locUseTex) const;
