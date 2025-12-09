1d1a998a1b0a5252ae5289e23263c920ec6cc641
Luka <lukaguledani123@gmail.com>
2025-12-09 18:15:46 +0400

    create a camera

---
+++ src/camera.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Luka on 09.12.2025.
+//
+#include "./headers/camera.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <GLFW/glfw3.h>
+
+Camera::Camera(const glm::vec3 position)
+{
+    Position = position;
+    Front = glm::vec3(0.0f, 0.0f, -1.0f);
+    Up = glm::vec3(0.0f, 1.0f, 0.0f);
+    Yaw = -90.0f;
+    Pitch = 0.0f;
+    Speed = 2.5f;
+    Sensitivity = 0.1f;
+}
+
+void Camera::ProcessKeyboard(int key, float deltaTime)
+{
+    const float velocity = Speed * deltaTime;
+    if (key == GLFW_KEY_W)
+        Position += Front * velocity;
+    if (key == GLFW_KEY_S)
+        Position -= Front * velocity;
+    if (key == GLFW_KEY_A)
+        Position -= glm::normalize(glm::cross(Front, Up)) * velocity;
+    if (key == GLFW_KEY_D)
+        Position += glm::normalize(glm::cross(Front, Up)) * velocity;
+}
+
+void Camera::ProcessMouseMovement(float xoffset, float yoffset)
+{
+    xoffset *= Sensitivity;
+    yoffset *= Sensitivity;
+
+    Yaw += xoffset;
+    Pitch += yoffset;
+
+    if (Pitch > 89.0f) Pitch = 89.0f;
+    if (Pitch < -89.0f) Pitch = -89.0f;
+
+    Front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    Front.y = sin(glm::radians(Pitch));
+    Front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    Front = glm::normalize(Front);
+}
+
+glm::mat4 Camera::GetViewMatrix() const {
+    return glm::lookAt(Position, Position + Front, Up);
+}
---
+++ src/headers/camera.h
@@ -0,0 +1,26 @@
+//
+// Created by Luka on 09.12.2025.
+//
+
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera
+{
+public:
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    float Yaw;
+    float Pitch;
+    float Speed;
+    float Sensitivity;
+
+    Camera(glm::vec3 position = glm::vec3(-0.111342f, 0.173541f, -1.19045f));
+
+
+    void ProcessKeyboard(int key, float deltaTime);
+    void ProcessMouseMovement(float xoffset, float yoffset);
+    glm::mat4 GetViewMatrix() const;
+};
